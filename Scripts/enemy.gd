661a04e9a4f1ee7768f459f15facef09f5d6f875
DeshpandeVedant37 <deshpandevedant37@gmail.com>
2023-11-11 11:41:09 +0530

    safety before redoing the enemy scene

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -4,14 +4,18 @@ var player_chase = false
 var player= null
 
 func _physics_process(delta):
+	var enemy = global_position
+	
+	print(enemy)
 	if player_chase == true:
-		position += (player.position - position) / speed
-	print (player)
-
+		position = player.position
+	move_and_slide()
 func _on_detection_area_body_entered(body):
-	player = body
-	player_chase = true
+	if body.is_in_group("Player"):
+		player = body
+		print (player.position)
+		player_chase = true
 
 func _on_detection_area_body_exited(body):
-	player = null
-	player_chase = false
+		player_chase = false
+		print (player.position)
